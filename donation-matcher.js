bcf1ec84f3b04f9dec66fcff74c6978ea6115c80
Madan Kumar Epuri <epurimk@yahooinc.com>
2025-06-16 10:45:21 +0530

    add upcoming goshala project in js

--- donation-matcher.js
+++ donation-matcher.js
@@ -57,7 +57,7 @@ const donationForms = {
     ihf: "https://secure.kbmandir.org/forms/recurring-sqft-seva-ihf"
   },
   "upcoming goshala-project": {
-    label: "Upcoming Goshala Project",
+    label: "Goshala - Upcoming Project",
     img: "https://www.kbgoshala.org/wp-content/uploads/2017/11/Screenshot-2024-06-17-at-8.04.18%E2%80%AFPM-2048x1324.png",
     vpf: "https://secure.kbmandir.org/forms/upcoming-goshala-project-vpf",
     ihf: "https://secure.kbmandir.org/forms/upcoming-goshala-project-ihf"
