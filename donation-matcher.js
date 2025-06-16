3b0e0c6178c68a1379fbd6c3414eda808f35e31d
Madan Kumar Epuri <epurimk@yahooinc.com>
2025-06-16 10:42:37 +0530

    add upcoming goshala project in js

--- donation-matcher.js
+++ donation-matcher.js
@@ -56,6 +56,12 @@ const donationForms = {
     vpf: "https://secure.kbmandir.org/forms/recurring-sqft-seva-vpf",
     ihf: "https://secure.kbmandir.org/forms/recurring-sqft-seva-ihf"
   },
+  "upcoming goshala-project": {
+    label: "Upcoming Goshala Project",
+    img: "https://www.kbgoshala.org/wp-content/uploads/2017/11/Screenshot-2024-06-17-at-8.04.18%E2%80%AFPM-2048x1324.png",
+    vpf: "https://secure.kbmandir.org/forms/upcoming-goshala-project-vpf",
+    ihf: "https://secure.kbmandir.org/forms/upcoming-goshala-project-ihf"
+  },
   "general-donation": {
     label: "General Donation",
     img: "https://www.kbmandir.org/wp-content/uploads/2020/11/Krishna-Balaram-Mahamantra.jpg",
