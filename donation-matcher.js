908d9f3cade1e9c11e2903c62941aba7405ab7e7
Madan Kumar Epuri <epurimk@yahooinc.com>
2025-06-19 12:17:45 +0530

    Use secure kbmandir urls for images

--- donation-matcher.js
+++ donation-matcher.js
@@ -28,19 +28,19 @@ const donationForms = {
   },
   "sri-lakshmi-narasimha-homa": {
     label: "Sri Lakshmi Narasimha Homa",
-    img: "https://d2r0txsugik6oi.cloudfront.net/neon/resource/ihf/images/lakshmi_narasimha.jpeg",
+    img: "https://secure.kbmandir.org/neon/resource/ihf/images/lakshmi_narasimha.jpeg",
     vpf: "https://secure.kbmandir.org/forms/sri-lakshmi-narasimha-homa-vpf",
     ihf: "https://secure.kbmandir.org/forms/sri-lakshmi-narasimha-homa-ihf"
   },
   "garland-sevas": {
     label: "Deity Garlands Seva",
-    img: "https://d2r0txsugik6oi.cloudfront.net/neon/resource/ihf/images/garland_sevas.jpeg",
+    img: "https://secure.kbmandir.org/neon/resource/ihf/images/garland_sevas.jpeg",
     vpf: "https://secure.kbmandir.org/forms/garland-sevas-vpf",
     ihf: "https://secure.kbmandir.org/forms/garland-sevas-ihf"
   },
   "nitya-prasada-seva-sponsorships": {
     label: "Nitya Prasada Seva",
-    img: "https://d2r0txsugik6oi.cloudfront.net/neon/resource/ihf/images/_J1A2922.JPG",
+    img: "https://secure.kbmandir.org/neon/resource/ihf/images/_J1A2922.JPG",
     vpf: "https://secure.kbmandir.org/forms/nitya-prasada-seva-sponsorships-vpf",
     ihf: "https://secure.kbmandir.org/forms/nitya-prasada-seva-sponsorships-ihf"
   },
@@ -52,13 +52,13 @@ const donationForms = {
   },
   "cow-feeding-seva": {
     label: "Goshala - Cow Feeding Seva",
-    img: "https://d2r0txsugik6oi.cloudfront.net/neon/resource/ihf/images/WhatsApp%20Image%202025-01-25%20at%2018_32_50.jpeg",
+    img: "https://secure.kbmandir.org/neon/resource/ihf/images/WhatsApp%20Image%202025-01-25%20at%2018_32_50.jpeg",
     vpf: "https://secure.kbmandir.org/forms/cow-feeding-seva-vpf",
     ihf: "https://secure.kbmandir.org/forms/cow-feeding-seva-ihf"
   },
   "recurring-sqft-seva": {
     label: "Goshala - Square Feet Seva",
-    img: "https://d2r0txsugik6oi.cloudfront.net/neon/resource/ihf/images/Sqft.jpg",
+    img: "https://secure.kbmandir.org/neon/resource/ihf/images/Sqft.jpg",
     vpf: "https://secure.kbmandir.org/forms/recurring-sqft-seva-vpf",
     ihf: "https://secure.kbmandir.org/forms/recurring-sqft-seva-ihf"
   },
