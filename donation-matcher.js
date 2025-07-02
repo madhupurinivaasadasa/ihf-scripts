9f1000a8fc7b419b712d7407fd57286ae8a4fc0a
Madan Kumar Epuri <epurimk@yahooinc.com>
2025-07-02 11:04:26 +0530

    change to 16x10 aspect ration

--- donation-matcher.js
+++ donation-matcher.js
@@ -10,7 +10,7 @@ const employerList = [
 const donationForms = {
    "jnm-2025": {
      label: "Janmashtami 2025 - Saturday, August 16th",
-     img: "https://secure.kbmandir.org/neon/resource/ihf/images/jnm_2025_flyer_portrait.jpg",
+     img: "https://secure.kbmandir.org/neon/resource/ihf/images/jnm_2025_flyer_updated.jpeg",
      vpf: "https://secure.kbmandir.org/forms/janmashtami-2025-vpf",
      ihf: "https://secure.kbmandir.org/forms/janmashtami-2025-ihf"
    },
