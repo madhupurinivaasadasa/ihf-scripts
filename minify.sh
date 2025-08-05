7cf12e5cb8d01278bb9638349e6836889853becc
Madan Kumar Epuri <epurimk@yahooinc.com>
2025-08-05 06:16:38 -0700

    add separate css for donations_wpadmin

--- minify.sh
+++ minify.sh
@@ -1,4 +1,4 @@
 terser donation-matcher.js -o donation-matcher.min.js -c -m
 terser donation-matcher-goshala.js -o donation-matcher-goshala.min.js -c -m
 cleancss -o donations.min.css donations.css
-cleancss -o donations_wpadmin.css donations_wpadmin.css
+cleancss -o donations_wpadmin.min.css donations_wpadmin.css
