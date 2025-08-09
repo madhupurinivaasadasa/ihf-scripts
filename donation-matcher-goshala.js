8d99df8734d295ada4244e400b8f3ed1aacca8d7
Madan Kumar Epuri <epurimk@yahooinc.com>
2025-08-09 11:53:34 -0700

    rename Sponsorship

--- donation-matcher-goshala.js
+++ donation-matcher-goshala.js
@@ -9,19 +9,19 @@ const employerList = [
 
 const donationForms = {
     "goshala-project": {
-        label: "Goshala Project- Seva Sponsorship Opportunity",
+        label: "Goshala Project- Seva Sponsorships",
         img: "https://www.kbgoshala.org/wp-content/uploads/2017/11/Screenshot-2024-06-17-at-8.04.18%E2%80%AFPM-2048x1324.png",
         vpf: "https://secure.kbmandir.org/forms/goshala-project-vpf",
         ihf: "https://secure.kbmandir.org/forms/goshala-project-ihf"
     },
     "cow-feeding-seva": {
-        label: "Cow Feeding Seva Sponsorship Opportunity",
+        label: "Cow Feeding Seva Sponsorships",
         img: "https://secure.kbmandir.org/neon/resource/ihf/images/WhatsApp%20Image%202025-01-25%20at%2018_32_50.jpeg",
         vpf: "https://secure.kbmandir.org/forms/cow-feeding-seva-vpf",
         ihf: "https://secure.kbmandir.org/forms/cow-feeding-seva-ihf"
     },
     "sastra-seva": {
-        label: "Sastra Dana - Seva Sponsorship Opportunity",
+        label: "Sastra Dana - Seva Sponsorships",
         img: "https://secure.kbmandir.org/neon/resource/ihf/images/book_distribution.jpeg",
         vpf: "https://secure.kbmandir.org/forms/book-distribution-vpf",
         ihf: "https://secure.kbmandir.org/forms/book-distribution-ihf"
