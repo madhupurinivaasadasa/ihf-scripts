ce36da1cbade36a2492a8fdde4c6b8c71d5ae6ec
Madan Kumar Epuri <epurimk@yahooinc.com>
2025-08-09 09:30:24 -0700

    Change seva sponsorship names

--- donation-matcher.js
+++ donation-matcher.js
@@ -9,73 +9,67 @@ const employerList = [
 
 const donationForms = {
     "jnm-2025": {
-        label: "Janmashtami 2025 - Saturday, August 16th",
+        label: "Janmashtami 2025 - Seva Sponsorship Form",
         img: "https://secure.kbmandir.org/neon/resource/ihf/images/jnm_2025_flyer_updated.jpeg",
         vpf: "https://secure.kbmandir.org/forms/janmashtami-2025-vpf",
         ihf: "https://secure.kbmandir.org/forms/janmashtami-2025-ihf"
     },
-    "balaram-jayanti": {
-        label: "Balarama Jayanti - Friday, August 08, 2025",
-        img: "https://secure.kbmandir.org/neon/resource/ihf/images/Balaram_Jayanti_2025.png",
-        vpf: "https://secure.kbmandir.org/forms/kbm-festival-seva-vpf",
-        ihf: "https://secure.kbmandir.org/forms/kbm-festival-seva-ihf"
-    },
     "new-temple-project": {
-        label: "New Temple Project Donations",
+        label: "New Temple Project - Seva Sponsorship Form",
         img: "https://www.kbmandir.org/wp-content/uploads/2017/11/nvw-scaled.jpg",
         vpf: "https://secure.kbmandir.org/forms/new-temple-project-donations-vpf",
         ihf: "https://secure.kbmandir.org/forms/new-temple-project-donations-ihf"
     },
     "sunday-feast": {
-        label: "Sponsor Sunday Feast",
+        label: "Sunday Feast Sponsorship Form",
         img: "https://secure.kbmandir.org/neon/resource/ihf/images/sunday_feast.JPG",
         vpf: "https://secure.kbmandir.org/forms/sunday-feast-sponsorship-vpf",
         ihf: "https://secure.kbmandir.org/forms/sunday-feast-sponsorship-ihf"
     },
     "sri-lakshmi-narasimha-homa": {
-        label: "Sri Lakshmi Narasimha Homa",
+        label: "Sri Lakshmi Narasimha Homa Sponsorship Form",
         img: "https://secure.kbmandir.org/neon/resource/ihf/images/narsimha_homa.jpg",
         vpf: "https://secure.kbmandir.org/forms/sri-lakshmi-narasimha-homa-vpf",
         ihf: "https://secure.kbmandir.org/forms/sri-lakshmi-narasimha-homa-ihf"
     },
     "garland-seva": {
-        label: "Deity Garlands Seva",
+        label: "Deity Garlands - Seva Sponsorship Form",
         img: "https://secure.kbmandir.org/neon/resource/ihf/images/garland_seva.JPG",
         vpf: "https://secure.kbmandir.org/forms/garland-sevas-vpf",
         ihf: "https://secure.kbmandir.org/forms/garland-sevas-ihf"
     },
     "nitya-prasada-seva": {
-        label: "Nitya Prasada Seva",
+        label: "Nitya Prasada Seva Sponsorship Form",
         img: "https://secure.kbmandir.org/neon/resource/ihf/images/_J1A2922.JPG",
         vpf: "https://secure.kbmandir.org/forms/nitya-prasada-seva-sponsorships-vpf",
         ihf: "https://secure.kbmandir.org/forms/nitya-prasada-seva-sponsorships-ihf"
     },
     "deity-seva": {
-        label: "Daily Deity Sevas",
+        label: "Daily Deity Seva Sponsorship Form",
         img: "https://www.kbmandir.org/wp-content/uploads/2022/03/2022_14.jpg",
         vpf: "https://secure.kbmandir.org/forms/deity-sevas-vpf",
         ihf: "https://secure.kbmandir.org/forms/deity-sevas-ihf"
     },
     "sastra-seva": {
-        label: "Sastra Dana Seva",
+        label: "Sastra Dana - Seva Sponsorship Form",
         img: "https://secure.kbmandir.org/neon/resource/ihf/images/book_distribution.jpeg",
         vpf: "https://secure.kbmandir.org/forms/book-distribution-vpf",
         ihf: "https://secure.kbmandir.org/forms/book-distribution-ihf"
     },
     "cow-feeding-seva": {
-        label: "Goshala - Cow Feeding Seva",
+        label: "Goshala - Cow Feeding Seva Sponsorship Form",
         img: "https://secure.kbmandir.org/neon/resource/ihf/images/WhatsApp%20Image%202025-01-25%20at%2018_32_50.jpeg",
         vpf: "https://secure.kbmandir.org/forms/cow-feeding-seva-vpf",
         ihf: "https://secure.kbmandir.org/forms/cow-feeding-seva-ihf"
     },
     "goshala-project": {
-        label: "Goshala Project Donations",
+        label: "Goshala Project - Seva Sponsorship Form",
         img: "https://www.kbgoshala.org/wp-content/uploads/2017/11/Screenshot-2024-06-17-at-8.04.18%E2%80%AFPM-2048x1324.png",
         vpf: "https://secure.kbmandir.org/forms/goshala-project-vpf",
         ihf: "https://secure.kbmandir.org/forms/goshala-project-ihf"
     },
     "general-donation": {
-        label: "General Donations",
+        label: "General Donations Form",
         img: "https://secure.kbmandir.org/neon/resource/ihf/images/general_donations.jpeg",
         vpf: "https://secure.kbmandir.org/forms/general-donation-vpf",
         ihf: "https://secure.kbmandir.org/forms/general-donation-ihf"
