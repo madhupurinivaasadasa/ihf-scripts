46073805c489c2b623ab68d66cf9bf893bcd2030
Madan Kumar Epuri <epurimk@yahooinc.com>
2025-09-28 15:07:58 -0700

    update goshala donations

--- donation-matcher-goshala.js
+++ donation-matcher-goshala.js
@@ -9,28 +9,28 @@ const employerList = [
 
 const donationForms = {
     "goshala-project": {
-        label: "Goshala Project - Pitru Paksha Seva Sponsorships",
+        label: "Goshala Project",
         img: "https://www.kbgoshala.org/wp-content/uploads/2017/11/Screenshot-2024-06-17-at-8.04.18%E2%80%AFPM-2048x1324.png",
         vpf: "https://secure.kbmandir.org/forms/goshala-project-vpf",
         ihf: "https://secure.kbmandir.org/forms/goshala-project-ihf"
     },
     "cow-feeding-seva": {
-        label: "Cow Feeding - Pitru Paksha Seva Sponsorships",
+        label: "Cow Feeding",
         img: "https://secure.kbmandir.org/neon/resource/ihf/images/WhatsApp%20Image%202025-01-25%20at%2018_32_50.jpeg",
         vpf: "https://secure.kbmandir.org/forms/cow-feeding-seva-vpf",
         ihf: "https://secure.kbmandir.org/forms/cow-feeding-seva-ihf"
+    },
+    "general-donation": {
+        label: "KBGoshala - General Donations",
+        img: "https://secure.kbmandir.org/neon/resource/ihf/images/general_donations.jpeg",
+        vpf: "https://secure.kbmandir.org/forms/goshala-general-donation-vpf",
+        ihf: "https://secure.kbmandir.org/forms/goshala-general-donation-ihf"
     },
     "sastra-seva": {
         label: "Sastra Dana - Seva Sponsorships",
         img: "https://secure.kbmandir.org/neon/resource/ihf/images/book_distribution.jpeg",
         vpf: "https://secure.kbmandir.org/forms/book-distribution-vpf",
         ihf: "https://secure.kbmandir.org/forms/book-distribution-ihf"
-    },
-    "general-donation": {
-        label: "KBMandir - General Donations",
-        img: "https://secure.kbmandir.org/neon/resource/ihf/images/general_donations.jpeg",
-        vpf: "https://secure.kbmandir.org/forms/general-donation-vpf",
-        ihf: "https://secure.kbmandir.org/forms/general-donation-ihf"
     },
 };
 
